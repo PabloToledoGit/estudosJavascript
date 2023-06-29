6e3de1643658df30363b5ae73b89b9ee3595c35f
PabloToledoGit <pablotoledoemail@gmail.com>
2023-06-29 14:04:51 -0300

    estudando variaveis

---
+++ projeto-atual/meuPrimeiroPrograma.js
@@ -0,0 +1,23 @@
+/*testando funcionalidade de variaveis e a nomeclatura do javascript*/
+
+console.log("meu primeiro software: trabalhando com variaveis");
+
+const idade = 26;
+
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+
+const idadeSubtraida = idade-2;
+console.log(idadeSubtraida)
+const idadeDividida = idade/2;
+console.log(idadeDividida)
+const idadeMultiplicada = idade*2;
+console.log(idadeMultiplicada)
