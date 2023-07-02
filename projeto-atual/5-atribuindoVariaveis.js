fa1ec3c9d2142b020853f601bd9c2e431ad094eb
PabloToledoGit <pablotoledoemail@gmail.com>
2023-07-02 10:52:44 -0300

    estudando variaveis

--- projeto-atual/5-atribuindoVariaveis.js
+++ projeto-atual/5-atribuindoVariaveis.js
@@ -3,16 +3,18 @@ console.log("Trabalhando com Atribuição de Variáveis");
 
 
 const idade = 29;
-let nome = "Ricardo";
+let primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
 
-console.log(nome, sobrenome);
-console.log(nome + " " +  sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(primeiroNome + " " +  sobrenome);
 
-console.log(`Meu nome é ${nome} ${sobrenome}`);
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 
-const nomeCompleto = nome + sobrenome;
+let contador = 0;
+contador = contador + 1;
 
+const nomeCompleto = primeiroNome + sobrenome;
 
 console.log(nomeCompleto);
 
