d5f5f2b260c042e769321faf81a0f8bdf91958c0
PabloToledoGit <pablotoledoemail@gmail.com>
2023-07-11 12:48:00 -0300

    estudando

--- projeto-atual/8-loops.js
+++ projeto-atual/8-loops.js
@@ -1,12 +1,14 @@
 console.log("Trabalhando com Condicionais");
 
 const listaDeDestinos = new Array("são paulo", "rio de janeiro", "salvador");
-const idadeComprador = 17;
+const idadeComprador = 18;
 console.log("Destinos Possiveis: \n");
 console.log(listaDeDestinos);
 
-const estaAcompanhada = false;
-let passagemComprada = false;
+const estaAcompanhada = true;
+let passagemComprada = true;
+const destino = "são paulo";
+
 
 if (idadeComprador >= 18 || estaAcompanhada == true) {
   listaDeDestinos.splice(1, 1);
@@ -16,11 +18,21 @@ if (idadeComprador >= 18 || estaAcompanhada == true) {
   console.log("Não é maior de idade, não posso vender \n");
   passagemComprada = false;
 }
-console.log("Embarque: \n")
-if(idadeComprador == 18 && passagemComprada){
+console.log("Embarque: \n");
+if (idadeComprador == 18 && passagemComprada) {
   console.log("Boa Viagem!!");
-
-}else{
+} else {
   console.log("Não é possivel embarcar \n");
 }
+let contador = 0;
+while (contador < 3) {
+  if(listaDeDestinos[contador] == destino){
+    console.log("Destino Existe");
 
+  }else{
+
+    console.log("Destino não existe")
+  }
+
+  contador += 1;
+}
